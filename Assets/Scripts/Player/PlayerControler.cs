749b276e67360b9e4cdf86a3cffa517a53aa95f6
adamkulik <hamstapl@gmail.com>
2016-10-16 21:03:44 +0200

    Fixed PlayerControler. Fixed axes in input manager so that movement is more responsive. Fixed jumping (temporary).

--- Assets/Scripts/Player/PlayerControler.cs
+++ Assets/Scripts/Player/PlayerControler.cs
@@ -4,11 +4,15 @@ using Helper;
 
 public class PlayerControler : MonoBehaviour {
     private Rigidbody rigid;
+	private Collider coll;
     public float speed = 5.0f;
     public float mouseSpeed = 10.0F;
+	public float jumpForce = 200.0F;
+	private bool jumped = false;
     // Use this for initialization
     void Start () {
-        rigid = GetComponent<Rigidbody>();
+        rigid = GetComponentInParent<Rigidbody>();
+
 	}
 
     void FixedUpdate()
@@ -19,12 +23,24 @@ public class PlayerControler : MonoBehaviour {
         float mouseX = Input.GetAxis(InputPlayer.MOUSEX);
         float mouseY = Input.GetAxis(InputPlayer.MOUSEY);
 
-        Vector3 movement = new Vector3(moveHorizontal, 3*jump, moveVertical);
-        rigid.velocity = movement * speed;
+		rigid.AddRelativeForce(Vector3.forward * moveVertical * speed);
+		rigid.AddRelativeForce(Vector3.right * moveHorizontal * speed);
+		if (!jumped) {
+			rigid.AddRelativeForce (Vector3.up * jump * jumpForce);
+			jumped = true;
+		}
+		Debug.Log (jump);
+		if (rigid.velocity.y == 0)
+			jumped = false;
 
 
         float h = (mouseSpeed * Input.GetAxis(InputPlayer.MOUSEX));
-        transform.Rotate(0, h, 0);
+		float v = (-1 * mouseSpeed * Input.GetAxis (InputPlayer.MOUSEY));
+        rigid.transform.Rotate(0, h, 0);
+		if (!((v > 0 && Vector3.Dot (transform.forward, Vector3.up) <= -0.85) 
+			|| (v < 0 && Vector3.Dot (transform.forward, Vector3.up) >= 0.85))) // stałe ustalone empirycznie, nie dotykać
+			transform.Rotate (v, 0, 0);
+		transform.eulerAngles = new Vector3 (transform.eulerAngles.x,transform.eulerAngles.y, 0);
 
     }
 }
