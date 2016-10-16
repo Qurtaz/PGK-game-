f3bf38a6b4c5ad0c58dc37e6b3bf5df0812470be
adamkulik <hamstapl@gmail.com>
2016-10-16 23:16:27 +0200

    Added setting platform height via mouse wheel, fixed platform base, refactored script

--- Assets/Scripts/TempBuild.cs
+++ Assets/Scripts/TempBuild.cs
@@ -3,27 +3,28 @@ using System.Collections;
 using Helper;
 
 public class TempBuild : MonoBehaviour {
-
+	private float scrolls;
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetAxis (InputPlayer.MOUSESCROLL) != 0)
+			scrolls += Input.GetAxis (InputPlayer.MOUSESCROLL);
 		Plane XZPlane = new Plane (Vector3.up, Vector3.zero);
 		float distance;
 		Vector3 hitPoint = new Vector3();
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		if (XZPlane.Raycast (ray, out distance)) {
 			hitPoint = ray.GetPoint (distance);
-			hitPoint.y = 0;
+			hitPoint.y = scrolls;
 		}
 		if (Input.GetAxis (InputPlayer.MOUSE0) > 0) {
 			GameObject newPlatform = Instantiate (Resources.Load ("Platform"), hitPoint, Quaternion.identity) as GameObject;
 			Destroy (gameObject);
 		}
-
 		transform.position = hitPoint;
 	}
 
