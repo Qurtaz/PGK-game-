e57cc40e1fededd28cd87161fbca56e8c217d1fa
adamkulik <hamstapl@gmail.com>
2016-10-29 17:02:25 +0200

    Removed graph structure from the scene. Added a system where player moves in a straight line towards target
    Player also jumps when it finds itself close to a higher target

--- Assets/Scripts/Player/PlayerControler.cs
+++ Assets/Scripts/Player/PlayerControler.cs
@@ -12,7 +12,9 @@ public class PlayerControler : MonoBehaviour {
 	private float distToGround;
 	private ResourceSystem player;
 	private Player cont;
+	private Vector3 hitPoint;
 	public bool blocked = false;
+	private bool moving = false;
     // Use this for initialization
     void Start () {
         rigid = GetComponentInParent<Rigidbody>();
@@ -23,26 +25,43 @@ public class PlayerControler : MonoBehaviour {
 
     void FixedUpdate()
     {
-		Plane XZPlane = new Plane(Vector3.up, Vector3.zero);
+		if (moving) {
+			rigid.transform.position = Vector3.MoveTowards (rigid.transform.position, hitPoint, speed * 2 * Time.deltaTime);
+			Vector3 diff = hitPoint - rigid.transform.position;
+			if (diff.x < 5.0f && diff.z < 5.0f && diff.y > 0.5f) {
+				float heightDiff = hitPoint.y - rigid.transform.position.y;
+				float v =Mathf.Sqrt( heightDiff / (2 * Physics.gravity.magnitude));
+				rigid.AddForce (new Vector3 (0, v*500, 0));
+			}
+			if (diff.magnitude < 0.5f)
+				moving = false;
+
+		}
+
+
 		float distance;
-		Vector3 hitPoint = new Vector3();
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
-		if (Physics.Raycast (ray, out hit)) {
-			hitPoint = hit.point;
-			hitPoint.x = Mathf.Round (hitPoint.x);
-			hitPoint.z = Mathf.Round (hitPoint.z);
-			hitPoint.y += 1F;
-		}
+	
 	
 		distance = Vector3.Distance (rigid.transform.position, hitPoint);
 
 		if (!blocked && Input.GetKeyDown(KeyCode.Mouse0) && !cont.outOfResources) {
+			if (Physics.Raycast (ray, out hit)) {
+				hitPoint = hit.point;
+				hitPoint.x = Mathf.Round (hitPoint.x);
+				hitPoint.z = Mathf.Round (hitPoint.z);
+				hitPoint.y += 1F;
+
+			}
 			player.UseResources (distance/5);
 			float heightDiff = hitPoint.y - rigid.transform.position.y;
 			if(heightDiff > 0)
 				player.UseResources (heightDiff);
-			rigid.transform.position = hitPoint;
+			moving = true;
+				
+				
+			
 		}
 		if (Input.GetKeyDown (KeyCode.Mouse1)) {
 			blocked = !blocked;
