57d50265029fdc18e239312ab88589961e52e12f
Bartazel <Bartazel@gmail.com>
2016-11-10 13:36:03 +0100

    Naprawiłem merga

--- Assets/Scripts/Building/CheckBuildPlatform.cs
+++ Assets/Scripts/Building/CheckBuildPlatform.cs
@@ -17,22 +17,24 @@ public class CheckBuildPlatform : MonoBehaviour {
 	{
 		builder.isAbleToBuild = false;
 		Debug.Log (builder.isAbleToBuild);
-	    if (other.gameObject.tag == "Platform")
-	    {
-            Debug.Log("NORACZEJ");
-	        builder.onPlatform = true;
-	        builder.offset = other.transform.position;
-            if(builder.destroy)
+        if (other.gameObject.tag == "Platform")
+        {
+            Debug.Log("DESTROY" + builder.destroy);
+            builder.onPlatform = true;
+            builder.offset = other.transform.position;
+            if (builder.destroy)
+            {
+                builder.destroy = false;
                 Destroy(other.gameObject);
-	        builder.destroy = false;
-	    }
-	}
+            }
+        }
+    }
 	void OnTriggerExit (Collider other)
 	{
 		builder.isAbleToBuild = true;
 		Debug.Log (builder.isAbleToBuild);
 	    builder.onPlatform = false;
-        builder.offset = new Vector3(0, 0, 0);
+       // builder.offset = new Vector3(0, 0, 0);
 	}
 
 }
--- Assets/Scripts/Building/TempBuildPlatform.cs
+++ Assets/Scripts/Building/TempBuildPlatform.cs
@@ -6,43 +6,27 @@ public class TempBuildPlatform : MonoBehaviour {
 	private float scrolls;
 	private Collider coll;
 	public bool isAbleToBuild = true;
-    public bool onPlatform = false;
 	private bool noCollisionInThisPoint = true;
 	private Card platformCard;
 	public float unit;
-<<<<<<< .mine
-    public bool destroy = false;
-    public Vector3 offset = new Vector3(0, 0, 0);
-=======
-    private bool flag = false;
->>>>>>> .r73
+    public bool onPlatform = false;
     private ControlerGame controller;
-	// Use this for initialization
-	void Start () {
+    public bool destroy = false;
+    private RaycastHit hit;
+    private bool iShouldBuild = false;
+    public Vector3 offset = new Vector3(0, 0, 0);
+    // Use this for initialization
+    void Start () {
 		platformCard = new BuildPlatformCard ();
 		controller = FindObjectOfType<ControlerGame> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetAxis (InputPlayer.MOUSESCROLL) != 0)
-			scrolls += Input.GetAxis (InputPlayer.MOUSESCROLL);
-<<<<<<< .mine
-		Vector3 hitPoint = new Vector3();
-		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		RaycastHit hit;
-		if (Physics.Raycast (ray, out hit)) {
-				hitPoint = hit.point;
-				hitPoint.x = grid.Round (hitPoint.x);
-				hitPoint.z = grid.Round (hitPoint.z);
-				//hitPoint.y = scrolls;
-				noCollisionInThisPoint = true; // bo jak ustawiamy tą flagę właśnie tak to trzeba ją przestawić z powrotem gdy już można wstawić tą platformę
-
-		}
-=======
+		//if (Input.GetAxis (InputPlayer.MOUSESCROLL) != 0)
+			//scrolls += Input.GetAxis (InputPlayer.MOUSESCROLL);
 		Vector3 hitPoint = MousePoint.mousePoint (unit);
-		//hitPoint.y = scrolls;
->>>>>>> .r72
+		hitPoint.y = scrolls;
 		GameObject[] gos;
 		gos = GameObject.FindGameObjectsWithTag ("Platform");
 		float minxz = Mathf.Infinity;
@@ -54,59 +38,39 @@ public class TempBuildPlatform : MonoBehaviour {
 		if (minxz == 0f) {
 			//transform.position = new Vector3 (10000, 10000, 10000);
 			noCollisionInThisPoint = false;
-<<<<<<< .mine
-            if (onPlatform)
-            {
-                hitPoint = offset + new Vector3(0, 2F, 0);
-            }
-            transform.position = hitPoint;
-        } else {
-=======
-            if (hit.collider.gameObject.tag == "Platform")
-            {
-                hitPoint = hit.collider.gameObject.transform.position + new Vector3(0, 2F, 0);
-            }
+
             transform.position = hitPoint;
         }
-        else {
->>>>>>> .r73
+        else
+        {
 			transform.position = hitPoint;
 			noCollisionInThisPoint = true;
-        }
+		}
 
-	    
-
-        if (Input.GetAxis (InputPlayer.MOUSE0) > 0 && isAbleToBuild && noCollisionInThisPoint) {
+		if (Input.GetAxis (InputPlayer.MOUSE0) > 0 && isAbleToBuild && noCollisionInThisPoint) {
 			Debug.Log (isAbleToBuild);
 			Instantiate (Resources.Load ("Platform"), hitPoint, Quaternion.identity);
 
 			Destroy (gameObject);
 		}
-<<<<<<< .mine
+
         if (Input.GetAxis(InputPlayer.MOUSE0) > 0 && !noCollisionInThisPoint && onPlatform)
         {
             Debug.Log(isAbleToBuild);
             destroy = true;
-            Instantiate(Resources.Load("Platform"), hitPoint, Quaternion.identity);
-
-            Destroy(gameObject);
-        }
-        if (Input.GetAxis (InputPlayer.MOUSE1) > 0) {
-=======
-        if (Input.GetAxis(InputPlayer.MOUSE0) > 0 && !noCollisionInThisPoint)
-        {
-            Debug.Log(isAbleToBuild);
-            if (hit.collider.gameObject.tag == "Platform")
+            Debug.Log("DESTROY" + destroy);
+            foreach (var go in gos)
             {
-                Destroy(hit.collider.gameObject);
-                Instantiate(Resources.Load("Platform"), hitPoint, Quaternion.identity);
-
-                Destroy(gameObject);
+                if (go.transform.position == gameObject.transform.position)
+                {
+                    Destroy(go);
+                    Instantiate(Resources.Load("Platform"), hitPoint + new Vector3(0, 2F, 0), Quaternion.identity);
+                    Destroy(gameObject);
+                }
             }
-            
         }
+
         if (Input.GetAxis (InputPlayer.MOUSE1) > 0) {
->>>>>>> .r73
 			controller.GiveResources (platformCard.cost);
 			controller.ReturnCardToPlayer (platformCard);
 			Destroy (gameObject);
