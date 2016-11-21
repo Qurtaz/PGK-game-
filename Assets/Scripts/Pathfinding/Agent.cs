5f0505964016e445ff51150c69b0cb51a2bd5565
adamkulik <hamstapl@gmail.com>
2016-11-21 21:19:29 +0100

    Added jumping to pathfinding agent

--- Assets/Scripts/Pathfinding/Agent.cs
+++ Assets/Scripts/Pathfinding/Agent.cs
@@ -12,13 +12,17 @@ public class Agent : MonoBehaviour {
 	private int i;
 	private bool moving;
 	public float speed = 5.0f;
+	public float force = 100f;
 	public NodeSwapPathfindingTest test;
+	private bool jumped;
+	private float distToGround;
 	// Use this for initialization
 	void Start () {
 		rigid = GetComponent<Rigidbody> ();
 		curTarget = null;
 		pathfinder = new ShortestRoute ();
 		test = GameObject.Find ("NodeTest").GetComponent<NodeSwapPathfindingTest>();
+		distToGround = gameObject.GetComponent<Collider> ().bounds.extents.y;
 	}
 	
 	// Update is called once per frame
@@ -32,8 +36,18 @@ public class Agent : MonoBehaviour {
 			rigid.transform.eulerAngles = new Vector3 (0,transform.eulerAngles.y, 0);
 			rigid.transform.position = Vector3.MoveTowards (rigid.transform.position, moveFlat, speed  * Time.deltaTime);
 			Vector3 diff = hitPoint - rigid.transform.position;
+	
+			if (diff.y > 0.1f && !jumped) {
+				Vector3 totalForce = Vector3.up* force * Mathf.Sqrt (diff.y / 2 * Physics.gravity.magnitude);
+				rigid.AddForce (totalForce,ForceMode.Impulse);
+				Vector3 forwardForce = new Vector3(diff.x,0,diff.z) * 5 *speed;
+				rigid.AddForce (forwardForce);
+				jumped = true;
+			}
+			if(Physics.Raycast(transform.position, Vector3.down, distToGround + 0.1f))
+				jumped = false;
 
-			if (diff.magnitude < 0.1f) {
+			if (diff.magnitude < 1.0f) {
 				rigid.transform.position = hitPoint;
 				i -= 1;
 				if(i!=0)
