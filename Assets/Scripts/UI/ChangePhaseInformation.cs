2a422774f463c9651798a8cc95f20e85bcfe3893
Qurtaz <piwowino1994@gamil.com>
2016-11-28 14:59:13 +0100

    Dodanie ekrazu z informacjiami

---
+++ Assets/Scripts/UI/ChangePhaseInformation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChangePhaseInformation : MonoBehaviour {
+    public ControlerGame gameControler;
+    public GameObject changePhaseInfo;
+    public float time = 2;
+    private Text _text;
+	// Use this for initialization
+	void Start () {
+        _text = changePhaseInfo.GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void See()
+    {
+        changePhaseInfo.SetActive(true);
+        _text.text = "Player turn: " + gameControler.GetPlayerTurn().ToString() +
+        "\n Phase: " + gameControler.GetPlayerPhase() + "\n Player name: " + gameControler.GetPlayerName();
+        Invoke("Turnoff", time);
+    }
+
+    private void Turnoff()
+    {
+        changePhaseInfo.SetActive(false);
+    }
+}
